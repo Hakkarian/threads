da21a2d85d20b687adf3f86317759d9dad48cfee
Hakkar <zugrutus@gmail.com>
2023-08-19 19:50:08 +0100

    Created frontend and backend for communities, added webhooks for connection to clerk

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,12 @@
+import ProfileHeader from "@/components/shared/ProfileHeader";
+
+const Page = async () => {
+  return (
+    <section>
+      <h1 className="head-text mb-10">Communities</h1>
+      {/* <ProfileHeader /> */}
+    </section>
+  );
+};
+
+export default Page;
